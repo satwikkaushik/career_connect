cebf0b97e424198e70b6cffac836c01e6a5e7b90
Satwik Kaushik <satwikkaushik05@gmail.com>
2025-02-22 06:55:36 +0000

    Create routes for student endpoint

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 import express from "express";
-import { router as homeRouter } from "./routes/home.js";
+import { router as homeRoute } from "./routes/home.js";
+import { router as studentRoute } from "./routes/student.js";
+import "dotenv/config";
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use("/", homeRouter);
+app.use("/", homeRoute);
+app.use("/student", studentRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
